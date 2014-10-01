c310b4848c7fbe926e02ee0c75a855716a5e57f7
Baptiste Deslaurier <baptiste.deslaurier@bts-malraux.net>
2014-10-01 14:12:02 +0000

    optimisation fonction validation

--- main.py
+++ main.py
@@ -1,36 +1,25 @@
 #!/usr/bin/python3
 ''' Fonction de vérification des sous arguments '''
-def checkSousArgs(arg, nomArg):
-    try:
-        # Conversion en entier
-        nb = int(arg[1])
-        # Si nb n'est pas un entier naturel et qu'il est supérieur ou égal à 100, on lève une exception !
-        if (checkIntNatural(nb) == False):
-            raise Exception('EntierNaturel',' --' + nomArg + ', la valeur "' + arg[1] + '" doit être positive !')
-        if (checkIntInfCent(nb) == False):
-            raise Exception('SuperieurACent', ' --' + nomArg + ', la valeur "' + arg[1] + '" doit être inférieure à "100" !')
-        # Si il n'y a pas d'erreur, on retourne la valeur 0
-        else:
-            return 0
-    except Exception as er:
-        if er.args[0] == 'EntierNaturel' and er.args[0] == 'SuperieurACent':
-            logging.error(er.args[1])
-        else:
-            logging.error(' --' + nomArg + ', impossible de convertir "' + arg[1] + '" en nombre entier !')
 
 ''' Vérifie qu'un nombre est un entier naturel '''
-def checkIntNatural(nb):
-    if nb > 0:
+def checkSousArgs2(arg):
+    logging.info("verifie argument 2")
+    try:
+        nb = int(arg[1])
+        if nb<0:
+            nb = abs(nb)
+            logging.warning('La quantité saisie doit etre positive')
+            logging.info('Nombre négatif transformé en positif: ' + str(nb))
+        elif nb>100:
+            nb = None
+            logging.warning('La quantité saisie est supérieur à 100')
+            logging.info('Nombre supérieur à 100 transformé en : ' + str(nb))
         return True
-    else:
-        return False
-    
-''' Vérifie qu'un nombre est inférieur à 100 '''
-def checkIntInfCent(nb):
-    if nb < 100:
-        return True
-    else:
-        return False
+    except ValueError:
+        print ("Impossible de convertir \"" + arg[1] + "\" en nombre entier !")
+        logging.error('Impossible de convertir ' + arg[1] + ' en nombre entier !')
+        logging.debug(' *****************************************')
+        exit(1)
 
 ''' Traitement du programme principal '''
 import argparse
@@ -78,9 +67,8 @@ for ARG in ['titre','genre','sousgenre','artiste','album']:
     if getattr(args, ARG) is not None:
         logging.info(' Argument --' + ARG + ' :\t' + getattr(args, ARG)[0] + ' ; ' + getattr(args, ARG)[1])
 # On vérifie que le 2em sous argument de genre est bien un entier naturel
-if checkSousArgs(args.genre,'genre') == 0:
+if checkSousArgs2(args.genre) == True:
     print ('ok')
-    
-    
+
 logging.debug(' *****************************************')
 # la commande exit(0) permet de quitter le programme sans omettre d'erreur, alors que exit(1) lève une erreur
